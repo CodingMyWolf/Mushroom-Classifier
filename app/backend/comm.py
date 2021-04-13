9f8a75294c9e061a2c6d7b0abdf14d66ebed61b7
Matteo Vidali <mvidali@iu.edu>
2021-04-13 03:16:34 -0400

    creating predict functionality

--- app/backend/comm.py
+++ app/backend/comm.py
@@ -1,15 +1,18 @@
 import os
+import predict
 from flask import jsonify, render_template, request, redirect, url_for
 from flask import send_file
 
 F_UP_F='../predict' # Form upload folder
 I_UP_F='../img_data'
 SERVER_DATABASE='../forms' # Form Download Folder
+UPLOAD_FOLDER='../uplaod'
 
 def upload_form(data):
     f = request.files['file']
     f.save(os.path.join(UPLOAD_FOLDER, 'predict.txt'))
-    return "Upload success"
+    p = predict.get_file(os.path.join(UPLOAD_FOLDER, 'predict.txt'))
+    return predict.predict(p)
 
 def download(data):
     path = os.path.join(SERVER_DATABASE, 'form.txt')
@@ -18,3 +21,5 @@ def download(data):
 def upload2():
   pass
 
+if __name__ == "__main__":
+  print(predict.get_file('../tests/form_FULL.txt'))
---
+++ app/backend/predict.py
@@ -0,0 +1,40 @@
+import pickle
+
+def predict(data):
+
+  filename = 'mushroom_random_forest.pkl'
+  i_f = open(filename, 'rb')
+  rf = pickle.load(i_f)
+  i_f.close()
+
+  s = "This mushroom is edible\n"
+  f = "This mushroom is poisonous\n"
+
+  return s if rf.predict(data) == 0 else f 
+
+def get_file(path):
+
+  with open(path, 'r') as f:
+    lines = f.readlines()
+    x, y, z  = lines[0], lines[1], lines[2] # x=odor, y=gillsize, z=bruises
+
+  _ ,x = x.split(':')
+  _ ,y = y.split(':')
+  _ ,z = z.split(':')
+
+  x = x[:-1].strip()
+  y = y[:-1].strip()
+  z = z[:-1].strip()
+
+  x_d = {'a':0, 'c':1, 'f':2, 'l':3, 'm':4, 'n':5, 'p':6, 's':7}
+  y_d = {'n':1, 'b':0}
+  z_d = {'t':1, 'f':0}
+
+  return [[x_d[x], y_d[y], z_d[z]]]
+
+if __name__ == "__main__":
+  print(  predict([[5,0,0]]) )
+  print(  predict([[5,4,5]]) )
+  x = get_file('../tests/form_FULL.txt')
+  print(x)
+  print(predict(x))
